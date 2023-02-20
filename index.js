350fadf5b51e3ad8489dc86212ff73463fbfc6fd
Ilya Ivashchenko <donhuilio@yandex.ru>
2023-02-20 04:41:31 +0300

    workaet

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ closePopup.addEventListener('click', () => {
   popupContainer.style.display = 'none'
 })
 
-// Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
+// // Находим форму в DOM
+// let formElement = document.querySelector('.name-output')
+// // Находим поля формы в DOM
 // let nameInput = // Воспользуйтесь инструментом .querySelector()
 // let jobInput = // Воспользуйтесь инструментом .querySelector()
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+// // Обработчик «отправки» формы, хотя пока
+// // она никуда отправляться не будет
+// function handleFormSubmit (evt) {
+//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+//                                                 // Так мы можем определить свою логику отправки.
+//                                                 // О том, как это делать, расскажем позже.
 
-    // Получите значение полей jobInput и nameInput из свойства value
+//     // Получите значение полей jobInput и nameInput из свойства value
 
-    // Выберите элементы, куда должны быть вставлены значения полей
+//     // Выберите элементы, куда должны быть вставлены значения полей
 
-    // Вставьте новые значения с помощью textContent
-}
+//     // Вставьте новые значения с помощью textContent
+// }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit)
+// // Прикрепляем обработчик к форме:
+// // он будет следить за событием “submit” - «отправка»
+// formElement.addEventListener('submit', handleFormSubmit)
