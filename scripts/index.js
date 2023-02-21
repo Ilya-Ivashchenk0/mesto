5d5aaa233e4562032bc1d4b0c370519c4258ca08
Ilya Ivashchenko <donhuilio@yandex.ru>
2023-02-21 15:21:01 +0300

    vse eshe verstka

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
 const openPopup = document.getElementById('open_popup')
 const closePopup = document.getElementById('close_popup')
-const popupContainer = document.getElementById('popup')
-let formElement = document.querySelector('.popup__container')
+const popupContainer = document.getElementById('popup_conteiner')
+let formElement = document.querySelector('.popup')
 let nameInput = document.querySelector('.popup__input_name')
 let jobInput = document.querySelector('.popup__input_job')
 
-openPopup.addEventListener('click', () => {
+function popupOpen () {
   popupContainer.style.display = 'flex'
-})
+}
 
-closePopup.addEventListener('click', () => {
+openPopup.addEventListener('click', popupOpen)
+
+function popupClose () {
   popupContainer.style.display = 'none'
-})
+}
+
+openPopup.addEventListener('click', popupClose)
 
 
 
