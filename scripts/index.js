75aa693cbeed3b67f618d4d3d379928831c01a15
Ilya Ivashchenko <donhuilio@yandex.ru>
2023-03-04 21:51:12 +0300

    func default card del

--- scripts/index.js
+++ scripts/index.js
@@ -16,37 +16,39 @@ let cardUrlInput = document.querySelector('.popup-card__input_field_url')
 // Выберите элементы, куда должны быть вставлены значения полей
 const profileName = document.querySelector('.profile__title')
 const profileAbout = document.querySelector('.profile__subtitle')
-
 const cardTemplate = document.querySelector('#template').content
-
 const cardsContainer = document.querySelector('.elements')
-
 const initialCards = [
   {
-    name: 'Шир',
-    link: 'https://staskulesh.com/wp-content/uploads/2012/05/nz_hobbiton_IMG_8526-800x533.jpg'
-  },
-  {
-    name: 'Ривенделл',
-    link: 'https://gamerwall.pro/uploads/posts/2022-03/1646211822_5-gamerwall-pro-p-gorod-elfov-vlastelin-kolets-rivendell-kra-5.jpg'
-  },
-  {
-    name: 'Врата Мории',
-    link: 'https://i.ibb.co/3rvpNfL/55181515.jpg'
-  },
-  {
-    name: 'Лес Фангорн',
-    link: 'https://pibig.info/uploads/posts/2022-12/1670277718_1-pibig-info-p-les-fangorn-krasivo-2.jpg'
+    name: 'Мордор',
+    link: 'https://upload.wikimedia.org/wikipedia/ru/6/6a/Mordor.png'
   },
   {
     name: 'Минас Тирит',
     link: 'https://static.wikia.nocookie.net/lotr/images/e/e4/Minas_Tirith.jpg/revision/latest?cb=20110408141647&path-prefix=ru'
   },
   {
-    name: 'Мордор',
-    link: 'https://upload.wikimedia.org/wikipedia/ru/6/6a/Mordor.png'
+    name: 'Лес Фангорн',
+    link: 'https://pibig.info/uploads/posts/2022-12/1670277718_1-pibig-info-p-les-fangorn-krasivo-2.jpg'
+  },
+  {
+    name: 'Врата Мории',
+    link: 'https://i.ibb.co/3rvpNfL/55181515.jpg'
+  },
+  {
+    name: 'Ривенделл',
+    link: 'https://gamerwall.pro/uploads/posts/2022-03/1646211822_5-gamerwall-pro-p-gorod-elfov-vlastelin-kolets-rivendell-kra-5.jpg'
+  },
+  {
+    name: 'Шир',
+    link: 'https://staskulesh.com/wp-content/uploads/2012/05/nz_hobbiton_IMG_8526-800x533.jpg'
   }
+
 ]
+
+function log (a) {
+  console.log(a)
+}
 
 function popupOpen () {
   popupContainer.classList.add('popup_opened')
@@ -83,10 +85,6 @@ function handleFormSubmit (evt) {
   popupClose()
 }
 
-
-
-
-
 const createCard = (data) => {
   // Клонируем шаблон, наполняем его информацией из объекта data, навешиваем всякие обработчики событий, о которых будет инфа ниже
   const cardCopy = cardTemplate.cloneNode(true)
@@ -99,8 +97,13 @@ const createCard = (data) => {
 const renderCard = (data) => {
   // Создаем карточку на основе данных
   const cardElement = createCard(data)
+
+
   // Помещаем ее в контейнер карточек
   cardsContainer.prepend(cardElement)
+
+
+  // Вешаем событие
 }
 
 function handleCardFormSubmit (evt) {
@@ -116,10 +119,16 @@ function handleCardFormSubmit (evt) {
   popupCardClose()
 }
 
-
 initialCards.forEach(card => { renderCard(card) })
 
 
+let trashButtons = document.querySelectorAll('.element__trash')
+
+trashButtons.forEach(function (card) {
+  card.addEventListener('click', function (evt) {
+    evt.target.closest('.element').remove()
+  })
+})
 
 closePopup.addEventListener('click', popupClose)
 openPopup.addEventListener('click', popupOpen)
