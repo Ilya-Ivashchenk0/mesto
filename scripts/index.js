4d6427e75880ecc12af7d894609d146adac90973
Ilya Ivashchenko <donhuilio@yandex.ru>
2023-03-10 16:40:54 +0300

    ostalsya 1 let

--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,50 @@
-const openPopup = document.getElementById('open_popup')
-const openPopupCard = document.getElementById('open_popup-card')
-const closePopup = document.getElementById('close_popup')
-const closePopupCard = document.getElementById('close_popup-card')
-const popupContainer = document.querySelector('.popup')
-const popupCardContainer = document.querySelector('.popup-card')
+//кнопки открытия и закрытия попапов
+const buttonOpenPopupProfile = document.getElementById('open_edit_popup_button')
+const buttonClosePopupProfile = document.getElementById('button_close_popup_profile')
+const buttonOpenPopupCard = document.getElementById('button_open_popup_card')
+const buttonClosePopupCard = document.getElementById('button_close_popup_card')
+const buttonClosePopupImg = document.getElementById('button_close_popup_img')
+//контейнеры попапов
+const popupProfileContainer = document.querySelector('.popup_type_profile')
+const popupCardContainer = document.querySelector('.popup_type_card')
+const popupImgContainer = document.querySelector('.popup_type_img')
 // Находим форму в DOM
-const formElement = document.querySelector('.popup__form')
-const cardFormElement = document.querySelector('.popup-card__form')
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_field_name')
-let jobInput = document.querySelector('.popup__input_field_job')
-
-let cardNameInput = document.querySelector('.popup-card__input_field_designation')
-let cardUrlInput = document.querySelector('.popup-card__input_field_url')
+const popupProfileForm = document.querySelector('.popup__form_place_profile')
+const popupCardForm = document.querySelector('.popup__form_place_card')
+// Находим поля форм в DOM
+const formNameInput = document.querySelector('.popup__input_field_name')
+const formJobInput = document.querySelector('.popup__input_field_job')
+const cardNameInput = document.querySelector('.popup__input_field_designation')
+const cardUrlInput = document.querySelector('.popup__input_field_url')
 // Выберите элементы, куда должны быть вставлены значения полей
 const profileName = document.querySelector('.profile__title')
-const profileAbout = document.querySelector('.profile__subtitle')
+const profileJob = document.querySelector('.profile__subtitle')
 const cardTemplate = document.querySelector('#template').content
 const cardsContainer = document.querySelector('.elements')
-const initialCards = [
-  {
-    name: 'Мордор',
-    link: 'https://upload.wikimedia.org/wikipedia/ru/6/6a/Mordor.png'
-  },
-  {
-    name: 'Минас Тирит',
-    link: 'https://static.wikia.nocookie.net/lotr/images/e/e4/Minas_Tirith.jpg/revision/latest?cb=20110408141647&path-prefix=ru'
-  },
-  {
-    name: 'Лес Фангорн',
-    link: 'https://pibig.info/uploads/posts/2022-12/1670277718_1-pibig-info-p-les-fangorn-krasivo-2.jpg'
-  },
-  {
-    name: 'Врата Мории',
-    link: 'https://i.ibb.co/3rvpNfL/55181515.jpg'
-  },
-  {
-    name: 'Ривенделл',
-    link: 'https://gamerwall.pro/uploads/posts/2022-03/1646211822_5-gamerwall-pro-p-gorod-elfov-vlastelin-kolets-rivendell-kra-5.jpg'
-  },
-  {
-    name: 'Шир',
-    link: 'https://staskulesh.com/wp-content/uploads/2012/05/nz_hobbiton_IMG_8526-800x533.jpg'
+
+let imgInfo
+
+function openPopup (data) {
+  if (data == popupProfileContainer) {
+    data.classList.add('popup_opened')
+    const profileTitleValue = profileName.textContent
+    const profileSubtitleValue = profileJob.textContent
+    formNameInput.value = `${profileTitleValue}`
+    formJobInput.value = `${profileSubtitleValue}`
+  } else if (data == imgInfo) {
+    popupImgContainer.classList.add('popup__place_img')
+    const imgPlace = popupImgContainer.querySelector('.popup__img')
+    popupImgContainer.classList.add('popup_opened')
+    imgPlace.setAttribute('src', data.src)
+    imgPlace.setAttribute('alt', data.alt)
+    popupImgContainer.querySelector('.popup__title_place_img').textContent = data.title
+  } else {
+    data.classList.add('popup_opened')
   }
-]
-
-function log (a) {
-  console.log(a)
 }
 
-function popupOpen () {
-  popupContainer.classList.add('popup_opened')
-  let profileTitleValue = profileName.textContent
-  let profileSubtitleValue = profileAbout.textContent
-  nameInput.value = `${profileTitleValue}`
-  jobInput.value = `${profileSubtitleValue}`
-}
-
-function popupClose () {
-  popupContainer.classList.remove('popup_opened')
-}
-
-function popupCardOpen () {
-  popupCardContainer.classList.add('popup-card_opened')
-}
-
-function popupCardClose () {
-  popupCardContainer.classList.remove('popup-card_opened')
+function closePopup (data) {
+  data.classList.remove('popup_opened')
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -76,12 +54,12 @@ function handleFormSubmit (evt) {
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
   // Получите значение полей jobInput и nameInput из свойства value
-  let nameValue = nameInput.value
-  let jobValue = jobInput.value
+  const nameValue = formNameInput.value
+  const jobValue = formJobInput.value
   // Вставьте новые значения с помощью textContent
   profileName.textContent = `${nameValue}`
-  profileAbout.textContent = `${jobValue}`
-  popupClose()
+  profileJob.textContent = `${jobValue}`
+  closePopup(popupProfileContainer)
 }
 
 const createCard = (data) => {
@@ -89,6 +67,24 @@ const createCard = (data) => {
   const cardCopy = cardTemplate.cloneNode(true)
   cardCopy.querySelector('.element__title').textContent = data.name
   cardCopy.querySelector('.element__mask-img').src = data.link
+  const trashButton = cardCopy.querySelector('.element__trash')
+  trashButton.addEventListener('click', function (evt) {
+    evt.target.closest('.element').remove()
+  })
+  const likesButton = cardCopy.querySelector('.element__button')
+  likesButton.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__button_color_black')
+  })
+  const cardCopyImg = cardCopy.querySelector('.element__mask-img')
+  cardCopyImg.addEventListener('click', function (evt) {
+    const thisCardImg = evt.target.closest('.element__mask-img')
+    imgInfo = {
+      src: thisCardImg.getAttribute('src'),
+      alt: thisCardImg.getAttribute('alt'),
+      title: evt.target.closest('.element').querySelector('.element__title').textContent
+    }
+    openPopup(imgInfo)
+  })
   // Возвращаем получившуюся карточку
   return cardCopy
 }
@@ -97,7 +93,7 @@ const renderCard = (data) => {
   // Создаем карточку на основе данных
   const cardElement = createCard(data)
   // Вешаем событие
-  let cardElemntPlace = cardElement.querySelector('.element__trash')
+  const cardElemntPlace = cardElement.querySelector('.element__trash')
   cardElemntPlace.addEventListener('click', function (evt) {
     evt.target.closest('.element').remove()
   })
@@ -107,60 +103,25 @@ const renderCard = (data) => {
 
 function handleCardFormSubmit (evt) {
   evt.preventDefault()
-  let enterInfo = {
+  const enterInfo = {
     name: `${cardNameInput.value}`,
     link: `${cardUrlInput.value}`
   }
   renderCard(enterInfo)
-  popupCardClose()
+  closePopup(popupCardContainer)
 }
 
 initialCards.forEach(card => { renderCard(card) })
 
-let trashButtons = document.querySelectorAll('.element__trash')
-let likesButtons = document.querySelectorAll('.element__button')
-const closePopupPlace = document.getElementById('close_popup-place')
-let openPlaceImg = document.querySelectorAll('.element__mask-img')
-const popupPlaceContainer = document.querySelector('.popup-place')
-
-likesButtons.forEach(function (button) {
-  button.addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__button_color_black')
-  })
-})
-
-trashButtons.forEach(function (card) {
-  card.addEventListener('click', function (evt) {
-    evt.target.closest('.element').remove()
-  })
-})
-
-function popupPlaceOpen (data) {
-  popupPlaceContainer.classList.add('popup-place_opened')
-  popupPlaceContainer.querySelector('.popup-place__img').setAttribute('src', data.src)
-  popupPlaceContainer.querySelector('.popup-place__img').setAttribute('alt', data.alt)
-  popupPlaceContainer.querySelector('.popup-place__title').textContent = data.title
-}
-
-function popupPlaceClose () {
-  popupPlaceContainer.classList.remove('popup-place_opened')
-}
-
-openPlaceImg.forEach(function (img) {
-  img.addEventListener('click', function (evt) {
-    let imgInfo = {
-      src: evt.target.closest('.element__mask-img').getAttribute('src'),
-      alt: evt.target.closest('.element__mask-img').getAttribute('alt'),
-      title: evt.target.closest('.element').querySelector('.element__title').textContent
-    }
-    popupPlaceOpen(imgInfo)
-  })
-})
-
-closePopup.addEventListener('click', popupClose)
-openPopup.addEventListener('click', popupOpen)
-formElement.addEventListener('submit', handleFormSubmit)
-closePopupCard.addEventListener('click', popupCardClose)
-openPopupCard.addEventListener('click', popupCardOpen)
-cardFormElement.addEventListener('submit', handleCardFormSubmit)
-closePopupPlace.addEventListener('click', popupPlaceClose)
+//кнопки попапа профиля
+buttonOpenPopupProfile.addEventListener('click', function() {openPopup(popupProfileContainer)})
+buttonClosePopupProfile.addEventListener('click', function() {closePopup(popupProfileContainer)})
+//кнопки попапа карточек
+buttonOpenPopupCard.addEventListener('click', function() {openPopup(popupCardContainer)})
+buttonClosePopupCard.addEventListener('click', function() {closePopup(popupCardContainer)})
+//кнопки попапа картинки
+buttonClosePopupImg.addEventListener('click', function() {openPopup(popupImgContainer)})
+buttonClosePopupImg.addEventListener('click', function() {closePopup(popupImgContainer)})
+//кнопки форм
+popupProfileForm.addEventListener('submit', handleFormSubmit)
+popupCardForm.addEventListener('submit', handleCardFormSubmit)
---
+++ scripts/initialCards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Мордор',
+    link: 'https://upload.wikimedia.org/wikipedia/ru/6/6a/Mordor.png'
+  },
+  {
+    name: 'Минас Тирит',
+    link: 'https://static.wikia.nocookie.net/lotr/images/e/e4/Minas_Tirith.jpg/revision/latest?cb=20110408141647&path-prefix=ru'
+  },
+  {
+    name: 'Лес Фангорн',
+    link: 'https://pibig.info/uploads/posts/2022-12/1670277718_1-pibig-info-p-les-fangorn-krasivo-2.jpg'
+  },
+  {
+    name: 'Врата Мории',
+    link: 'https://i.ibb.co/3rvpNfL/55181515.jpg'
+  },
+  {
+    name: 'Ривенделл',
+    link: 'https://gamerwall.pro/uploads/posts/2022-03/1646211822_5-gamerwall-pro-p-gorod-elfov-vlastelin-kolets-rivendell-kra-5.jpg'
+  },
+  {
+    name: 'Шир',
+    link: 'https://staskulesh.com/wp-content/uploads/2012/05/nz_hobbiton_IMG_8526-800x533.jpg'
+  }
+]
